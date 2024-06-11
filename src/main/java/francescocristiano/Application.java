f2d23aa165db81c37655d5091307b2155817e064
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-06-11 18:32:13 +0200

    Little fix in application

--- src/main/java/francescocristiano/Application.java
+++ src/main/java/francescocristiano/Application.java
@@ -1,12 +1,11 @@
 package francescocristiano;
 
 import jakarta.persistence.EntityManagerFactory;
-
-import static jakarta.persistence.Persistence.createEntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Application {
 
-    private static final EntityManagerFactory emf = createEntityManagerFactory("u4bwteam1");
+    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4bwteam1");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
