5fc2948e677611470ee0d8366dedbadf0bc61a7f
Francesco Cristiano <frankrys97@gmail.com>
2024-06-12 11:21:08 +0200

    adds PuntoVenditaDAO and ValidazioneDAO

--- src/main/java/francescocristiano/dao/PuntoVenditaDAO.java
+++ src/main/java/francescocristiano/dao/PuntoVenditaDAO.java
@@ -1,21 +1,33 @@
 package francescocristiano.dao;
 
+import francescocristiano.entities.puntiVendita.DistributoreAutomatico;
 import francescocristiano.entities.puntiVendita.PuntoVendita;
+import francescocristiano.entities.puntiVendita.Rivenditore;
+import francescocristiano.entities.titoliDiViaggio.Abbonamento;
+import francescocristiano.entities.titoliDiViaggio.Biglietto;
+import francescocristiano.entities.utenti.Tessera;
+import francescocristiano.entities.utenti.Utente;
+import francescocristiano.enums.StatusDistributore;
+import francescocristiano.enums.TipoAbbonamento;
 import francescocristiano.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class PuntoVenditaDAO {
 
     private final EntityManager em;
+    private final TitoloDiViaggioDAO titoloDiViaggioDAO;
 
     public PuntoVenditaDAO(EntityManager em) {
+
         this.em = em;
+        this.titoloDiViaggioDAO = new TitoloDiViaggioDAO(em);
     }
 
-    public void aggiungiPuntoVendita(PuntoVendita puntoVendita){
+    public void aggiungiPuntoVendita(PuntoVendita puntoVendita) {
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
         em.persist(puntoVendita);
@@ -27,5 +39,60 @@ public class PuntoVenditaDAO {
         PuntoVendita puntoVendita = em.find(PuntoVendita.class, id);
         if (puntoVendita == null) throw new NotFoundException(id);
         return puntoVendita;
+    }
+
+    public void emettiBiglietto(PuntoVendita puntoVendita) throws Exception {
+        if (puntoVendita instanceof Rivenditore rivenditore) {
+            if (!rivenditore.isLicenza()) {
+                throw new Exception("Il rivenditore non è in possesso di licenza");
+            }
+        } else if (puntoVendita instanceof DistributoreAutomatico distributoreAutomatico) {
+            if (distributoreAutomatico.getStato() != StatusDistributore.ATTIVO) {
+                throw new Exception("Il distributore è fuori servizio");
+            }
+        } else {
+            throw new Exception("Punto vendita non valido");
+        }
+
+        Biglietto bigliettoEmesso = new Biglietto(LocalDate.now(), puntoVendita);
+        titoloDiViaggioDAO.aggiungiTitoloDiViaggio(bigliettoEmesso);
+        System.out.println("Biglietto emesso con successo");
+    }
+
+    public void emettiAbbonamento(PuntoVendita puntoVendita, Utente utente, TipoAbbonamento tipoAbbonamento) throws Exception {
+        Tessera tessera = utente.getNumeroTessera();
+        if (tessera == null) {
+            throw new Exception("L'utente non possiede una tessera per l'abbonamento");
+        } else if (tessera.getDataScadenza().isBefore(LocalDate.now())) {
+            throw new Exception("La tessera dell'utente è scaduta");
+        }
+
+        if (puntoVendita instanceof Rivenditore rivenditore) {
+            if (!rivenditore.isLicenza()) {
+                throw new Exception("Il rivenditore non è in possesso di licenza");
+            }
+        } else if (puntoVendita instanceof DistributoreAutomatico distributoreAutomatico) {
+            if (distributoreAutomatico.getStato() != StatusDistributore.ATTIVO) {
+                throw new Exception("Il distributore è fuori servizio");
+            }
+        } else {
+            throw new Exception("Punto vendita non valido");
+        }
+
+        Abbonamento abbonamentoEmesso = new Abbonamento(LocalDate.now(), puntoVendita, tipoAbbonamento, tessera);
+        titoloDiViaggioDAO.aggiungiTitoloDiViaggio(abbonamentoEmesso);
+        System.out.println("Abbbonamento emesso con successo");
+    }
+
+    public long conteggioTitoliDiViaggioEmessiPerPuntoVendita(PuntoVendita puntoVendita) {
+        return em.createQuery("SELECT COUNT(t) FROM TitoloDiViaggio t WHERE t.puntoVendita = :puntoVendita", Long.class).setParameter("puntoVendita", puntoVendita).getSingleResult();
+    }
+
+    public long conteggioTitoliDiViaggioEmessiPerData(LocalDate dataInizio, LocalDate dataFine) {
+        return em.createQuery("SELECT COUNT(tv) FROM TitoloDiViaggio tv WHERE tv.dataEmissione BETWEEN :dataInizio AND :dataFine", Long.class).setParameter("dataInizio", dataInizio).setParameter("dataFine", dataFine).getSingleResult();
+    }
+
+    public long conteggioTitoliDiViaggioEmessiPerDataPerPuntoVendita(LocalDate dataInizio, LocalDate dataFine, PuntoVendita puntoVendita) {
+        return em.createQuery("SELECT COUNT(tv) FROM TitoloDiViaggio tv WHERE tv.dataEmissione BETWEEN :dataInizio AND :dataFine AND tv.puntoVendita = :puntoVendita", Long.class).setParameter("dataInizio", dataInizio).setParameter("dataFine", dataFine).setParameter("puntoVendita", puntoVendita).getSingleResult();
     }
 }
--- src/main/java/francescocristiano/dao/ValidazioneDAO.java
+++ src/main/java/francescocristiano/dao/ValidazioneDAO.java
@@ -1,9 +1,17 @@
 package francescocristiano.dao;
 
-import francescocristiano.entities.utenti.Utente;
+import francescocristiano.entities.mezzi.Mezzo;
+import francescocristiano.entities.titoliDiViaggio.Abbonamento;
+import francescocristiano.entities.titoliDiViaggio.Biglietto;
 import francescocristiano.entities.validazioni.Validazione;
+import francescocristiano.entities.validazioni.ValidazioneAbbonamento;
+import francescocristiano.entities.validazioni.ValidazioneBiglietto;
+import francescocristiano.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 public class ValidazioneDAO {
     private final EntityManager em;
@@ -12,11 +20,41 @@ public class ValidazioneDAO {
         this.em = em;
     }
 
-    public void aggiungiValidazione(Validazione validazione){
+    public void aggiungiValidazione(Validazione validazione) {
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
         em.persist(validazione);
         transaction.commit();
         System.out.println("Validazione salvata con successo nel database!");
     }
+
+    public Validazione findById(UUID id) {
+        Validazione validazione = em.find(Validazione.class, id);
+        if (validazione == null) throw new NotFoundException(id);
+        return validazione;
+    }
+
+    public void vidimazioneBiglietto(Biglietto biglietto, Mezzo mezzo) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        biglietto.setValido(false);
+        em.merge(biglietto);
+        ValidazioneBiglietto validazioneBiglietto = new ValidazioneBiglietto(mezzo, LocalDate.now(), biglietto);
+        em.persist(validazioneBiglietto);
+        transaction.commit();
+        System.out.println("Biglietto validato con successo");
+    }
+
+    public void vidimazioneAbbonamento(Abbonamento abbonamento, Mezzo mezzo) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        ValidazioneAbbonamento validazioneAbbonamento = new ValidazioneAbbonamento(mezzo, LocalDate.now(), abbonamento);
+        em.persist(validazioneAbbonamento);
+
+        transaction.commit();
+        System.out.println("Abbonamento validato con successo");
+    }
+
+
 }
